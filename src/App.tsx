ed0d37c0b0ca85699071da169616f2b7ed552ec8
Kelvin Mark <kelvinmark619@gmail.com>
2025-10-24 17:55:21 +0100

    Fixed foooter
    Removed footer section from the main application.

--- src/App.tsx
+++ src/App.tsx
@@ -28,9 +28,6 @@ const App: React.FC = () => {
           <Route path="/todos/:id" element={<TodoDetails />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-        <footer>
-          <p>Built by Odunayo Alt School Exam Project</p>
-        </footer>
       </ErrorBoundary>
     </Router>
   );
